e37a85fffb9a3ef673303dab0a74fe27093387b8
TheoCaesar <theocaesar7@gmail.com>
2022-09-19 14:18:08 -0700

    task t5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *  * rev_string - check the code
+ *   * @s: param s
+ *    * Return: Always 0.
+ */
+void rev_string(char *s)
+{
+	int i;
+	int count = 0;
+	char rev_s[1000];
+	int num = 0;
+	int count_2 = count;
+	int c;
+
+	for (i = 0; ; i++)
+	{
+		if (s[i] == '\0')
+			break;
+		count++;
+	}
+
+	while (count)
+	{
+		rev_s[num] = s[count - 1];
+		count--;
+		num++;
+	}
+
+	for (c = 0; c < count_2; c++)
+	{
+		if (rev_s[c] == '\0')
+			break;
+		*(s + c) = rev_s[c];
+	}
+}
