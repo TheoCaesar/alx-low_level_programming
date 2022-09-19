651f1a88f23bd75054f2d53416f633ee58b74e7e
TheoCaesar <theocaesar7@gmail.com>
2022-09-19 14:46:05 -0700

    task 5alpha

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -3,16 +3,16 @@
 
 /**
  *  * rev_string - check the code
- *   * @s: param s
+ * @s: param
  *    * Return: Always 0.
  */
 void rev_string(char *s)
 {
 	int i;
 	int count = 0;
+	int count_2 = 0;
 	char rev_s[1000];
 	int num = 0;
-	int count_2 = count;
 	int c;
 
 	for (i = 0; ; i++)
@@ -21,6 +21,8 @@ void rev_string(char *s)
 			break;
 		count++;
 	}
+
+	count_2 = count;
 
 	while (count)
 	{
