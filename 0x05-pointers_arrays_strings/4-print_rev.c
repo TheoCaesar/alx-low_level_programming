a26d0a7a66017676311bbae158210a7972310fd0
TheoCaesar <theocaesar7@gmail.com>
2022-09-19 13:47:41 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * * print_rev - check the code
+ * * @s: param s
+ * * Return: Always 0.
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0; ; i++)
+	{
+		if (s[i] == '\0')
+			break;
+		count++;
+	}
+
+	int count_2 = count;
+	char rev_s[count];
+	int num = 0;
+
+	while (count)
+	{
+		rev_s[num] = s[count - 1];
+		count--;
+		num++;
+	}
+
+	int c;
+
+	for (c = 0; c < count_2; c++)
+	{
+		if (rev_s[c] == '\0')
+			break;
+		putchar(rev_s[c]);
+	}
+
+	putchar('\n');
+}
+
