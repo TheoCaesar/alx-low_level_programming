cfa947c39e42272b43da9813286d6e168c3634f7
TheoCaesar <theocaesar7@gmail.com>
2022-09-19 15:16:42 -0700

    last

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,13 +10,15 @@
 void puts2(char *str)
 {
 	int i;
+	int count = 0;
 
 	for (i = 0; ; i++)
 	{
-		if (str[i] == '\0')
+		if (str[i] == '\0' || count == 224)
 			break;
 		putchar(str[i]);
 		i++;
+		count++;
 	}
 	putchar('\n');
 }
